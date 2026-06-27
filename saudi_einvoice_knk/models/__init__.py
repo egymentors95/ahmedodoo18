1e4ffe5a30b10836ca2081ff369594a79e4a16ea
mohamed <ibs.developer1@gmail.com>
2026-06-27 16:28:55 +0300

    /

--- saudi_einvoice_knk/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Powered by Kanak Infosystems LLP.
-# © 2020 Kanak Infosystems LLP. (<https://www.kanakinfosystems.com>).
-
-from . import saudi_invoice
-from . import res_partner
--- saudi_einvoice_knk/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    building_number = fields.Char(string="Building Number")
-    unit_number = fields.Char(string="Unit Number")
-    additional_no = fields.Char(string="Additional Number")
-    cr_no = fields.Char(string="CR Number")
